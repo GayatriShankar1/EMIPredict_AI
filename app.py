5615e4f4037ece53c38ce4eafb591ca54015a373
Gayatri Shankar <gayatrishankar@Gayatris-MacBook-Air.local>
2026-09-05 07:49:09 +0530

    Use gdown fuzzy URL download method

--- app.py
+++ app.py
@@ -8,21 +8,15 @@ os.makedirs("models", exist_ok=True)
 classifier_path = "models/classifier.pkl"
 regressor_path = "models/regressor.pkl"
 
-# Download classifier using its File ID
+# Download classifier using direct URL and fuzzy matching
 if not os.path.exists(classifier_path):
-    gdown.download(
-        id="1aD3GQ7uWyaeN5x9j4p2cpV1F8nuwenyr",
-        output=classifier_path,
-        quiet=False,
-    )
+    url_clf = "https://drive.google.com/file/d/1aD3GQ7uWyaeN5x9j4p2cpV1F8nuwenyr/view?usp=sharing"
+    gdown.download(url_clf, classifier_path, quiet=False, fuzzy=True)
 
-# Download regressor using its File ID
+# Download regressor using direct URL and fuzzy matching
 if not os.path.exists(regressor_path):
-    gdown.download(
-        id="1iWT2Oot62sfbJxbCTJCbxPMCh_kgh9hf",
-        output=regressor_path,
-        quiet=False,
-    )
+    url_reg = "https://drive.google.com/file/d/1iWT2Oot62sfbJxbCtJCbxPMCh_kgh9hf/view?usp=share_link"
+    gdown.download(url_reg, regressor_path, quiet=False, fuzzy=True)
 
 classifier = joblib.load(classifier_path)
 regressor = joblib.load(regressor_path)
